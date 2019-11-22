187d4e4b98c899b263eb62f1e431f09e2d95ca0d
harshavardhanak <53828700+harshavardhanak@users.noreply.github.com>
2019-11-22 15:35:52 +0530

    Add files via upload

---
+++ Synchr.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Callme
+	{
+	synchronized void call(String msg)
+		{
+		System.out.print("["+msg);
+		try
+			{
+			Thread.sleep(1000);
+			}
+		catch(InterruptedException e)
+			{
+			System.out.println("Caught Exception");
+			}
+		System.out.println("]");
+		}
+	}
+
+class Caller implements Runnable
+	{
+	String msg;
+	Callme target;
+	Thread t;
+
+	public Caller(Callme targ, String s)
+		{
+		target=targ;
+		msg=s;
+		t=new Thread(this);
+		t.start();
+		}
+
+	public void run()
+		{
+		target.call(msg);
+		}
+	}
+
+class Synchr
+	{
+	public static void main(String args[])
+		{
+		Callme target=new Callme();
+		Caller ob1=new Caller(target,"hello");
+		Caller ob2=new Caller(target,"syn");
+		Caller ob3=new Caller(target,"World");
+
+		try
+			{
+			ob1.t.join();
+			ob2.t.join();
+			ob3.t.join();
+			}
+		catch(InterruptedException e)
+			{
+			System.out.println("Interrupted");
+			}
+		}
+	}
+
